4639ace99a823db1ff5c58d328422f4012bff718
GM-MP-2023 <mpdevs@outlook.com>
2025-02-02 19:17:37 -0300

    copia patients

---
+++ PatientCare.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using PatientCare.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientCare.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByUsernameAsync(string email);
+        Task AddUserAsync(User user);
+    }
+}
+
